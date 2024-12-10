075efc6b23259a747ea198ccd58049176148d7aa
Nutsy-Cuckoo <yesheydorjee4@gmail.com>
2024-12-10 09:57:36 +0600

    HMS-16: Implemented refresh token endpoints, refresh token generation, CORS and made some changes to the CRUD APIs

--- src/main/java/com/Java/hotelmanagementsystem/user/service/UserService.java
+++ src/main/java/com/Java/hotelmanagementsystem/user/service/UserService.java
@@ -8,6 +8,7 @@ import com.Java.hotelmanagementsystem.util.constants.UserConstants;
 import com.Java.hotelmanagementsystem.user.model.User;
 import com.Java.hotelmanagementsystem.user.repository.UserRepository;
 import com.Java.hotelmanagementsystem.util.exception.GlobalExceptionWrapper;
+import io.micrometer.common.util.StringUtils;
 import jakarta.transaction.Transactional;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +40,7 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public UserDTO save(@lombok.NonNull User user) {
+    public UserDTO save(@NonNull User user) {
         //Check if same user already exists during signup
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new GlobalExceptionWrapper.BadRequestException(DUPLICATE_EMAIL_MESSAGE);
@@ -52,48 +53,70 @@ public class UserService implements IUserService {
         return UserMapper.toDTO(savedUser);
     }
 
+
     @Override
-    public UserDTO findById(long id) throws Exception {
-        return null;
+    public UserDTO fetchById(long id) {
+        User user = findById(id);
+        return UserMapper.toDTO(user);
     }
 
+    private User findById(long id) {
+        return this.userRepository.findById(id).orElseThrow(
+                () -> new GlobalExceptionWrapper.NotFoundException(String.format(NOT_FOUND_MESSAGE,
+                        USER.toLowerCase())));
+    }
 
-    public UserDTO fetchSelfInfo() {
+    @Override
+    public User fetchSelfInfo() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String email = ((UserInfoDetails) authentication.getPrincipal()).getUsername();
-        return  findByEmail(email).orElseThrow(
-                () -> new GlobalExceptionWrapper.NotFoundException(String.format(NOT_FOUND_MESSAGE, USER.toLowerCase())));
+        return findByEmail(email).orElseThrow(
+                () -> new GlobalExceptionWrapper.NotFoundException(String.format(NOT_FOUND_MESSAGE,
+                        USER.toLowerCase())));
     }
 
-    public Optional<UserDTO> findByEmail(@lombok.NonNull String emailId) {
-        Optional<User> user = this.userRepository.findByEmail(emailId);
-        return UserMapper.toDTO(user);
+    public Optional<User> findByEmail(@NonNull String emailId) {
+        return this.userRepository.findByEmail(emailId);
     }
 
     @Override
-    public UserDTO findById(long id) {
-        User user = this.userRepository.findById(id).orElseThrow(
-                () -> new GlobalExceptionWrapper.NotFoundException(String.format(NOT_FOUND_MESSAGE, USER.toLowerCase())));
-        return UserMapper.toDTO(user);
-    }
-  
-    @Override
-    public String update(long id, User entity) {
-        return "";
+    public String update(long id, @NonNull UserDTO userDTO) {
+        User authenticatedUser = fetchSelfInfo();
+
+        //Allow update by admin to the user info.
+        if (Arrays.stream(authenticatedUser.getRoles().split(",")).anyMatch(role -> role.trim().equalsIgnoreCase(
+                "ADMIN"))) {
+            authenticatedUser = findById(id);
+        }
+
+        if (StringUtils.isNotBlank(userDTO.getCid())) {
+            authenticatedUser.setCid(userDTO.getCid());
+        }
+
+        if (StringUtils.isNotBlank(userDTO.getName())) {
+            authenticatedUser.setName(userDTO.getName());
+        }
+
+        if (StringUtils.isNotBlank(userDTO.getPhone())) {
+            authenticatedUser.setPhone(userDTO.getPhone());
+        }
+
+        this.userRepository.save(authenticatedUser);
+        return String.format(UPDATED_SUCCESSFULLY_MESSAGE, USER);
     }
 
     @Override
     @Transactional
     public String deleteById(long id) {
-        UserDTO authenticatedUser = fetchSelfInfo();
-        User userEntity = UserMapper.toEntity(authenticatedUser);
+        User authenticatedUser = fetchSelfInfo();
 
-        //Allow to delete by admin to the instructor info.
-        if(Arrays.stream(authenticatedUser.getRoles().split(",")).anyMatch(role -> role.trim().equalsIgnoreCase("ADMIN"))){
-            userEntity = UserMapper.toEntity(findById(id));
+        //Allow to delete by admin to the user info.
+        if (Arrays.stream(authenticatedUser.getRoles().split(",")).anyMatch(role -> role.trim().equalsIgnoreCase(
+                "ADMIN"))) {
+            authenticatedUser = findById(id);
         }
 
-        this.userRepository.deleteById(userEntity.getId());
+        this.userRepository.deleteById(authenticatedUser.getId());
         return String.format(DELETED_SUCCESSFULLY_MESSAGE, USER);
     }
 }
